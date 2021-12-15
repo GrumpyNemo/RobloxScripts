a1ea61900c3a6cda1343109de264c5560e3d6c4b
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2021-12-15 17:48:45 -0500

    Create GunModz.lua

---
+++ ROBLOX/Fortnite/GunModz.lua
@@ -0,0 +1,9 @@
+local Gun = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
+local Config = Gun:WaitForChild("Configuration")
+Config.RecoilMin.Value = 0
+Config.RecoilMax.Value = 0
+Config.RecoilDecay.Value = 0
+Config.TotalRecoilMax.Value = 0
+Config.MaxSpread.Value = 0
+Config.GravityFactor.Value = 0
+Config.ShotCooldown.Value = 0
