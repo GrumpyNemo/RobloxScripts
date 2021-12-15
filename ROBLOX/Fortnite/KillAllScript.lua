f4b84a2db7ae91f137af7e150e27196ccb874eb4
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2021-12-15 17:42:26 -0500

    Update KillAllScript.lua

--- ROBLOX/Fortnite/KillAllScript.lua
+++ ROBLOX/Fortnite/KillAllScript.lua
@@ -4,7 +4,7 @@ local KillPlayer = function(plrName)
     local Weapon = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
     local args = {[1] = Weapon,[2] = {["p"] = Vector3.new(0, 0, 0),["pid"] = 1,["part"] = Player.Character.Head,["d"] = 7,["maxDist"] = 0,["h"] = Player.Character.Humanoid,["m"] = Enum.Material.Plastic,["sid"] = 6,["t"] = 0,["n"] = Vector3.new(0, 0, 0)}}
     local Counter = 0
-while Counter <= 5 do
+while Counter <= 7 do
     game:GetService("ReplicatedStorage").WeaponsSystem.Network.WeaponHit:FireServer(unpack(args))
     Counter = Counter+1
     
