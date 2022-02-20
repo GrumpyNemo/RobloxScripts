26839e07d404dfdef693d21a2dfffd156edeae09
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-02-19 19:36:45 -0500

    Update whitelists.lua

--- ScriptStuff/whitelists.lua
+++ ScriptStuff/whitelists.lua
@@ -1,6 +1,7 @@
 local Module = {}
 Module.KeySystem = {
   {1588640466,"N3M0__.QU@;J0BbO3"},
-  {570066736,"N3M0__h!xK]wH#AO]C"}
+  {570066736,"N3M0__h!xK]wH#AO]C"},
+  {74098652,"N3M0__.QU@;J0BbO3"}
 }
 return Module
