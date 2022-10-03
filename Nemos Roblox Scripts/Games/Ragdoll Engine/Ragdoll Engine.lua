01a443fa11cf0c32c1828820784146f6fd2b3e57
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-10-03 19:59:02 -0400

    Update Ragdoll Engine.lua

--- Nemos Roblox Scripts/Games/Ragdoll Engine/Ragdoll Engine.lua
+++ Nemos Roblox Scripts/Games/Ragdoll Engine/Ragdoll Engine.lua
@@ -1,53 +1,5 @@
---Created by Nemo / game: https://www.roblox.com/games/8892207911/Ragdoll-Engine
-local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
-local Char = Player.Character
-local PushTool = Player.Backpack:FindFirstChild("Push") or Char:FindFirstChild("Push")
-local PushedScript = Char:FindFirstChild("Pushed")
+--Created by Nemo / game: https://www.roblox.com/games/5683833663/Ragdoll-Engine
+local LP = game.Players.LocalPlayer
+local LPChar = LP.Character
 
-local function GetClosest()
-    local Character = Char
-    local HumanoidRootPart = Character and Character:FindFirstChild("HumanoidRootPart")
-    if not (Character or HumanoidRootPart) then return end
-
-    local TargetDistance = math.huge
-    local Target
-
-    for i,v in ipairs(Players:GetPlayers()) do
-        if v ~= Player and v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
-            local TargetHRP = v.Character.HumanoidRootPart
-            local mag = (HumanoidRootPart.Position - TargetHRP.Position).magnitude
-            if mag < TargetDistance then
-                TargetDistance = mag
-                Target = v
-            end
-        end
-    end
-    if TargetDistance > 4 then
-        else
-        return Target  
-    end
-end
-Spawn(function()
-    while wait() do
-        if PushedScript then
-            PushedScript:Destroy()
-        end
-    end
-end)
-
-Spawn(function()
-    while wait() do
-        if PushTool.Parent == Char then
-            local ClosestPlayer = GetClosest()
-            if ClosestPlayer then
-                if ClosestPlayer.ClassName == "Player" then
-                    print(ClosestPlayer.Name)
-                    PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])
-                    PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])
-                    PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])PushTool.PushTool:FireServer(game.Workspace[ClosestPlayer.Name])
-                end
-            end
-        end
-    end
-end)
+LPChar:FindFirstChild("Local Ragdoll").Enabled = false
