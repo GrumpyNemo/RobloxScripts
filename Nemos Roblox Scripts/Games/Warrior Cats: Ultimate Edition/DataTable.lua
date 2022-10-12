43d7945540b6a73a4bbc99a21b7cd5187b8b834e
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-10-11 22:40:50 -0400

    Update DataTable.lua

--- Nemos Roblox Scripts/Games/Warrior Cats: Ultimate Edition/DataTable.lua
+++ Nemos Roblox Scripts/Games/Warrior Cats: Ultimate Edition/DataTable.lua
@@ -8,4 +8,26 @@ data.Teams = {
     "Kittypets",
     "Rogues & Loners"
 }
+data.Sizes = {
+    "Kit",
+    "Apprentice",
+    "Young Warrior",
+    "Warrior",
+    "Large Warrior"
+}
+data.Roles {
+    "Warrior",
+    "Apprentice",
+    "Kit",
+    "Medicine Cat",
+    "Medicine Apprentice",
+    "Elder",
+    "Queen",
+    "Deputy",
+    "Leader",
+    "Rogue",
+    "Loner",
+    "Kittypet"
+    
+}
 return data
