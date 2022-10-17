eaddec96feda019623e4a4f7910e82f61968d34b
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-10-16 21:24:37 -0400

    Update Ragdoll Engine.lua

--- Nemos Roblox Scripts/Games/Ragdoll Engine/Ragdoll Engine.lua
+++ Nemos Roblox Scripts/Games/Ragdoll Engine/Ragdoll Engine.lua
@@ -1,5 +1,2 @@
 --Created by Nemo / game: https://www.roblox.com/games/5683833663/Ragdoll-Engine
-local LP = game.Players.LocalPlayer
-local LPChar = LP.Character
-
-LPChar:FindFirstChild("Local Ragdoll").Enabled = false
+game.Players.LocalPlayer.Character:FindFirstChild("Local Ragdoll").Enabled = not game.Players.LocalPlayer.Character:FindFirstChild("Local Ragdoll").Enabled
