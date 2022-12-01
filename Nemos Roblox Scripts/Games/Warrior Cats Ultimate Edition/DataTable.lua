087939228ea32d943052611323667d4007c5adaa
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-12-01 09:34:45 -0500

    Create DataTable.lua

---
+++ Nemos Roblox Scripts/Games/Warrior Cats Ultimate Edition/DataTable.lua
@@ -0,0 +1,33 @@
+data = {}
+data.Teams = {
+    "Creating Character",
+    "RiverClan",
+    "ShadowClan",
+    "ThunderClan",
+    "WindClan",
+    "Kittypets",
+    "Rogues & Loners"
+}
+data.Sizes = {
+    "Kit",
+    "Apprentice",
+    "Young Warrior",
+    "Warrior",
+    "Large Warrior"
+}
+data.Roles = {
+    "Warrior",
+    "Apprentice",
+    "Kit",
+    "Medicine Cat",
+    "Medicine Apprentice",
+    "Elder",
+    "Queen",
+    "Deputy",
+    "Leader",
+    "Rogue",
+    "Loner",
+    "Kittypet"
+    
+}
+return data
