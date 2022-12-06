c81cb7099d48e13fb8e68b26169ce9701a0f0a1c
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-12-06 14:24:02 -0500

    Update TablesLib.lua

--- Nemos Roblox Scripts/Games/Wolves Life 3/TablesLib.lua
+++ Nemos Roblox Scripts/Games/Wolves Life 3/TablesLib.lua
@@ -1,1 +1,20 @@
-
+lib = {}
+lib.ValidItems = {
+    --Toys
+    "Duck",
+    "Ball",
+    "Football",
+    "Bone",
+    "Stick",
+    --Items
+    "Torch",
+    "Potion",
+    --Foods
+    "Raw Venison",
+    "Raw Fish",
+    "Raw Chicken Leg",
+    "Raw Porkchop",
+    "Raw Beef",
+    "Raw Chicken Breast"
+}
+return lib
