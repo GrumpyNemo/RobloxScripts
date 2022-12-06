2af10f7547c33ab0aff65c53ded9776a91254656
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-12-06 11:20:50 -0500

    Delete Nemos Roblox Scripts/Games/Fortnite directory

--- Nemos Roblox Scripts/Games/Fortnite/GunModz.lua
+++
@@ -1,9 +0,0 @@
-local Gun = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
-local Config = Gun:WaitForChild("Configuration")
-Config.RecoilMin.Value = 0
-Config.RecoilMax.Value = 0
-Config.RecoilDecay.Value = 0
-Config.TotalRecoilMax.Value = 0
-Config.MaxSpread.Value = 0
-Config.GravityFactor.Value = 0
-Config.ShotCooldown.Value = 0
