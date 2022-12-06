c3b80f7d4b453c30eac9815756a8096e622d5c9b
Grumpy Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2022-12-06 11:21:08 -0500

    Delete Ragdoll Engine.lua

--- Nemos Roblox Scripts/Games/Ragdoll Engine/Ragdoll Engine.lua
+++
@@ -1,2 +0,0 @@
---Created by Nemo / game: https://www.roblox.com/games/5683833663/Ragdoll-Engine
-game.Players.LocalPlayer.Character:FindFirstChild("Local Ragdoll").Enabled = not game.Players.LocalPlayer.Character:FindFirstChild("Local Ragdoll").Enabled
