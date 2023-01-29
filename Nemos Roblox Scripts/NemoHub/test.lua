3fda41edcf7228e9ba1a09acc90abc1f61a5e8f6
Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2023-01-28 21:08:01 -0500

    Create test.lua

---
+++ Nemos Roblox Scripts/NemoHub/test.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GrumpyNemo/RobloxScripts/main/Nemos%20Roblox%20Scripts/NemoHub/main.lua'))()
+local pre = ("https://raw.githubusercontent.com/GrumpyNemo/RobloxScripts/main/Nemos%20Roblox%20Scripts/NemoHub/")
+local ext = (".lua")
+local construct = (pre..game.PlaceId..ext)
+wait()
+loadstring(game:HttpGet(tostring(construct)))()
