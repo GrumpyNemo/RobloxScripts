fa7686cccc36070b4000da3b6e8009fbc41b4afe
Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2023-01-29 17:41:23 -0500

    Create OMEN.lua

---
+++ Nemos Roblox Scripts/NemoHub/OMEN.lua
@@ -0,0 +1,4 @@
+getgenv().DeadzoneTab = nil
+getgenv().WolvesLife3Tab = nil
+getgenv().LaunchGames = true
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GrumpyNemo/RobloxScripts/main/Nemos%20Roblox%20Scripts/NemoHub/main.lua'))()
