857513bf3f89e3a6d0c3d68b2d634ca01ac2cc72
Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2023-01-30 21:07:01 -0500

    Update Highlighter.lua

--- libraries/Highlighter.lua
+++ libraries/Highlighter.lua
@@ -1,24 +1,37 @@
 local ModelHighlightModule = {}
+
+
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local LP = Players.LocalPlayer
+
 ---
-ModelHighlightModule.AlreadyHighlighted = {}
----
+
 function ModelHighlightModule.highlightModel(model,RGBColor)
+    
     if model.ClassName == "Model" then
+        
     pcall(function()
-            if not ModelHighlightModule.AlreadyHighlighted[model] then
+        
+            if not CoreGui:FindFirstChild(model.Name) then
+                
                 local ModelHighlight = Instance.new("Highlight", CoreGui)
                 ModelHighlight.Name = model.Name
                 ModelHighlight.Adornee = model
                 ModelHighlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
                 ModelHighlight.FillColor = RGBColor
-                ModelHighlightModule.AlreadyHighlighted[model] = ModelHighlight
+                
             else
-                ModelHighlightModule.AlreadyHighlighted[model].FillColor = RGBColor
+                
+                CoreGui:FindFirstChild(model.Name).FillColor = RGBColor
+                CoreGui:FindFirstChild(model.Name).Adornee = model
+                
             end
+            
     end)
+    
     end
 end
+
+
 return ModelHighlightModule
