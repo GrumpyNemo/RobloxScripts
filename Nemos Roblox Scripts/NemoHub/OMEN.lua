440244e2c5a796ede00e25f43555249f0d8cab90
Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2023-01-30 21:45:32 -0500

    Update OMEN.lua

--- Nemos Roblox Scripts/NemoHub/OMEN.lua
+++ Nemos Roblox Scripts/NemoHub/OMEN.lua
@@ -1,4 +1,22 @@
-getgenv().DeadzoneTab = nil
-getgenv().WolvesLife3Tab = nil
-getgenv().LaunchGames = true
-loadstring(game:HttpGet('https://raw.githubusercontent.com/GrumpyNemo/RobloxScripts/main/Nemos%20Roblox%20Scripts/NemoHub/main.lua'))()
+--<[[Grumpy Nemo]]>--
+                                                                                    --[[
+    Hi, this is honestly just some script hub that I'm gonna use for myself.
+    If you're anyone who wants to use this scrpt hub... go ahead
+    If you wanna use my scripts, go ahead.
+    But please credit me, unless the script is NOT mine.
+    Please do not credit me for someone else's script.                                  ]]
+    
+    printcredits = true
+    gamespecific = true
+--<[[URL]]>--
+
+omen_url = ('https://raw.githubusercontent.com/GrumpyNemo/RobloxScripts/main/Nemos%20Roblox%20Scripts/NemoHub/main.lua')
+
+--<[[Script]]>--
+
+getgenv().omen_advrefresh = true
+getgenv().LaunchGames = gamespecific
+getgenv().printcreds = printcredits
+loadstring(game:HttpGet(omen_url))()
+
+--<[[End of Script]]>--
