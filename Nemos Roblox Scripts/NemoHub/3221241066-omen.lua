3512e6ed27fe0f4fd0a08f7a578280cc761ca2de
Nemo <76014626+GrumpyNemo@users.noreply.github.com>
2023-03-07 23:55:00 -0500

    Update 3221241066-omen.lua

--- Nemos Roblox Scripts/NemoHub/3221241066-omen.lua
+++ Nemos Roblox Scripts/NemoHub/3221241066-omen.lua
@@ -81,8 +81,8 @@ if not getgenv().DeadzoneTab then
 		Name = "Set fly key",
 		CurrentKeybind = "G",
 		HoldToInteract = false,
-		Flag = "SetFlyingKeybind", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-		Callback = function(Keybind)
+		Flag = "SetFlyingKeybind",
+			Callback = function(Keybind)
 			DeadzoneFly.flytoggle(false)
 		end,
 	})
